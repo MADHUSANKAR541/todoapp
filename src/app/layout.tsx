30a72c1c1204f90a50dc0a4031898672f4ed6715
MADHUSANKAR541 <msmadhushankar@gmail.com>
2025-04-27 18:47:17 +0530

    hh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import {
   ClerkProvider,
   SignInButton,
@@ -29,6 +30,7 @@ export default function RootLayout({
       </head>
       <body >
         {children}
+        <Analytics/> 
       </body>
     </html>
     </ClerkProvider>
